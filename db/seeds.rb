547afc27b446d2362866495f7325d5baa1afb0d1
Lydia Lachenaud <lydia.lachenaud@gmail.com>
2022-05-16 20:58:26 +0200

    heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,26 +9,26 @@
 require 'faker'
 require 'devise'
 
-# 10.times do
-#     User.create!(
-#         first_name: Faker::Name.first_name,
-#         last_name:  Faker::Name.last_name,
-#         email: Faker::Name.first_name.concat("@yopmail.com"),
-#         encrypted_password: "valena",
-#         description: Faker::Quotes::Shakespeare.hamlet_quote)
-# end
+10.times do
+    User.create!(
+        first_name: Faker::Name.first_name,
+        last_name:  Faker::Name.last_name,
+        email: Faker::Name.first_name.concat("@yopmail.com"),
+        encrypted_password: "valena",
+        description: Faker::Quotes::Shakespeare.hamlet_quote)
+end
 
-# 5.times do
-#     Event.create!(
-#         start_date: Faker::Date.between(from: Date.today, to: 1.year.from_now),
-#         duration: rand(5..60)*5,
-#         title: Faker::Science.science,
-#         description: Faker::TvShows::Buffy.quote,
-#         price: rand(1..500),
-#         location: Faker::Address.street_address,
-#         admin: User.all.sample
-#     )
-# end
+5.times do
+    Event.create!(
+        start_date: Faker::Date.between(from: Date.today, to: 1.year.from_now),
+        duration: rand(5..60)*5,
+        title: Faker::Science.science,
+        description: Faker::TvShows::Buffy.quote,
+        price: rand(1..500),
+        location: Faker::Address.street_address,
+        admin: User.all.sample
+    )
+end
 
 Event.all.each do
 5.times do
