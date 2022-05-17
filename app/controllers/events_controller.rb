64b7fdfc8e9921882b2f7e760799c5b543d263e7
Lydia Lachenaud <lydia.lachenaud@gmail.com>
2022-05-17 05:11:51 +0200

    ajout show event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,13 +2,32 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
   def new
     @event = Event.new
   end
+
   def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to event_path(@event.id)
+    else
+      render :new
+    end
   end
+
   def show
     @event = Event.find(params[:id])
+    @attendees = Attendance.find_by(event_id: @event.id)
+    if @attendees
+      @ids = @attendees.pluck(:attendee_id)
+ 
+    end
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:start_date, :duration, :title, :description, :price, :location, :admin_id)
   end
 
 end
