1cd1e7044e4cdb34f418848c9f27e1a2f7804cc0
Lydia Lachenaud <lydia.lachenaud@gmail.com>
2022-05-17 10:10:53 +0200

    ajout apikey sendgrid

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ require 'devise'
         first_name: Faker::Name.first_name,
         last_name:  Faker::Name.last_name,
         email: Faker::Name.first_name.concat("@yopmail.com"),
-        encrypted_password: "valena",
+        password: "valena",
         description: Faker::Quotes::Shakespeare.hamlet_quote)
 end
 
